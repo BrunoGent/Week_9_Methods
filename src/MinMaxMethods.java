064f40bee88880c385500370df7392f051bbdafa
Bruno Gentiletti <brunogentiletti@gmail.com>
2022-12-10 01:08:21 +0000

    v1

--- src/MinMaxMethods.java
+++ src/MinMaxMethods.java
@@ -1,9 +1,4 @@
-// read 3 int noum1 num2 num3
-// 1 method return the minimum of 3 num
-//1 method return the max of 3 num
-
 import java.util.Scanner;
-
 public class MinMaxMethods {
     public static void main(String[] args) {
         Scanner Input = new Scanner(System.in);
@@ -13,18 +8,25 @@ public class MinMaxMethods {
         double num2 = Input.nextDouble();
         System.out.print("enter third number");
         double num3 = Input.nextDouble();
-        System.out.println("Lowest number is " + Lowest(num1,num2,num3));
-        System.out.println("Highest number is " + Highest(num1,num2,num3));
-
+        if (num1 % 1 == 0 && num2 % 1 == 0 && num3 % 1 == 0) {
+            System.out.println("Lowest number is " + Lowest((int)num1, (int)num2, (int)num3));
+            System.out.println("Highest number is " + Highest((int)num1, (int)num2, (int)num3));
+        }
+        else {
+            System.out.println("Lowest number is " + Lowest(num1, num2, num3));
+            System.out.println("Highest number is " + Highest(num1, num2, num3));
+        }
     }
     static double Lowest(double x , double y , double z){
-        //if (x < y && x < z){return x;}
-        //if (y < x && y < z){return y;}
         return Math.min(Math.min(x,y),z);
     }
     static double Highest(double x , double y , double z){
-        //if (x < y && x < z){return x;}
-        //if (y < x && y < z){return y;}
+        return Math.max(Math.max(x,y),z);
+    }
+    static int Lowest(int x , int y , int z){
+        return Math.min(Math.min(x,y),z);
+    }
+    static int Highest(int x , int y , int z){
         return Math.max(Math.max(x,y),z);
     }
 }
